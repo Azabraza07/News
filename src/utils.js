dbf966b11809288a37ed6d19d50747b2439323e7
azabraza_07 <azabraza_07@mail.ru>
2023-07-11 19:38:11 +0600

    first commit

---
+++ src/utils.js
@@ -0,0 +1,15 @@
+export const categoryIds = {
+  index: 0,
+  sport: 2,
+  technologies: 1,
+  politics: 4,
+  fashion: 3,
+};
+
+export const categoryNames = {
+  index: "Главная",
+  fashion: "Мода",
+  technologies: "Технологии",
+  politics: "Политика",
+  sport: "Спорт",
+};
